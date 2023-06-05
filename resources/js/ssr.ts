d37949f366259f4b362630b1610cbaa6dbab80cc
Ashesh Singh <ask@mrsinh.com>
2023-06-05 13:19:07 -0500

    refactor: switch to typescript

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,4 +1,4 @@
-import { createSSRApp, h } from 'vue';
+import { createSSRApp, h, DefineComponent } from 'vue';
 import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
@@ -12,12 +12,14 @@ createServer((page) =>
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
                 .use(ZiggyVue, {
+                    // @ts-expect-error
                     ...page.props.ziggy,
+                    // @ts-expect-error
                     location: new URL(page.props.ziggy.location),
                 });
         },
