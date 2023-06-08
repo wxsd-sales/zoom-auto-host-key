4a2611149c7ae7806e8e229b4c198a8b8d4f6e84
Ashesh Singh <ask@mrsinh.com>
2023-06-08 09:08:04 -0500

    feat: add rule for validating array or delimited string includes every element

---
+++ app/Rules/Has.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+use JsonException;
+
+class Has implements ValidationRule
+{
+    private readonly mixed $required;
+
+    private readonly ?string $valueSeparator;
+
+    /**
+     * @var callable|null
+     */
+    private $valueComparer;
+
+    public function __construct(mixed $required, ?string $valueSeparator = ' ', ?callable $valueComparer = null)
+    {
+        $this->required = gettype($required) === 'array' ? $required : [$required];
+        $this->valueSeparator = $valueSeparator;
+        $this->valueComparer = $valueComparer;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  Closure(string): PotentiallyTranslatedString  $fail
+     *
+     * @throws JsonException
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (gettype($value) === 'string' && $this->valueSeparator !== null) {
+            $value = explode($this->valueSeparator, $value);
+        } elseif (gettype($value) === 'string' && $this->valueSeparator === null) {
+            $value = json_decode($value, flags: JSON_THROW_ON_ERROR);
+        }
+
+        $value = gettype($value) === 'array' ? $value : [$value];
+
+        $missing = array_values($this->valueComparer !== null
+            ? array_udiff($this->required, $value, $this->valueComparer)
+            : array_diff($this->required, $value)
+        );
+
+        if (count($missing) > 0) {
+            $missingStr = implode(', ', array_map('json_encode', $missing));
+            $fail("The :attribute field must include $missingStr.");
+        }
+    }
+}
