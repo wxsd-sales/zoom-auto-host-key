26d065f211e050d8a5046529164368f4c7251bd7
Ashesh Singh <ask@mrsinh.com>
2023-06-23 12:18:04 -0500

    feat: default to server error pages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+//use Illuminate\Http\JsonResponse;
+//use Illuminate\Http\Response;
+//use Inertia\Inertia;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,4 +30,30 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+//    /**
+//     * Prepare exception for rendering.
+//     *
+//     * @param $request
+//     * @param Throwable $e
+//     *
+//     * @return JsonResponse|Response|\Symfony\Component\HttpFoundation\Response
+//     * @throws Throwable
+//     */
+//    public function render($request, Throwable $e): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response
+//    {
+//        $response = parent::render($request, $e);
+//
+//        if (! app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
+//            return Inertia::render('Error', ['status' => $response->status()])
+//                ->toResponse($request)
+//                ->setStatusCode($response->status());
+//        } elseif ($response->status() === 419) {
+//            return back()->with([
+//                'message' => 'The page expired, please try again.',
+//            ]);
+//        }
+//
+//        return $response;
+//    }
 }
