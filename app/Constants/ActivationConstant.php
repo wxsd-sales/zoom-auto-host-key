b415581a8b648ee68706c02f8dbc650797de18ef
Ashesh Singh <ask@mrsinh.com>
2023-06-26 12:18:04 -0500

    feat: add application constants

---
+++ app/Constants/ActivationConstant.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Constants;
+
+final class ActivationConstant
+{
+    public const WBX_WI_CLIENT_ID = 'wbxWiClientId';
+
+    public const WBX_WI_CLIENT_SECRET = 'wbxWiClientSecret';
+
+    public const WBX_WI_CONFIG = 'wbxWiConfig';
+
+    public const WBX_WI_JWT = 'wbxWiJwt';
+
+    public const WBX_WI_JWT_ERRORED = 'wbxWiJwtErrored';
+
+    public const WBX_WI_JWT_PAYLOAD = 'wbxWiJwtPayload';
+
+    public const WBX_WI_MANIFEST = 'wbxWiManifest';
+
+    public const WBX_WI_MANIFEST_ID = 'wbxWiManifestId';
+
+    public const WBX_WI_MANIFEST_VERSION = 'wbxWiManifestVersion';
+
+    public const WBX_WI_ORG_ID = 'wbxWiOrgId';
+
+    public const WBX_WI_DISPLAY_NAME = 'wbxWiDisplayName';
+
+    public const WBX_WI_MANIFEST_ERRORED = 'wbxWiManifestErrored';
+
+    public const WBX_WI_OAUTH = 'wbxWiOauth';
+
+    public const WBX_WI_OAUTH_ERRORED = 'wbxWiOauthErrored';
+
+    public const WBX_WI_REFRESH_TOKEN = 'wbxWiRefreshToken';
+
+    public const ZM_HOST_ACCOUNTS = 'zmHostAccounts';
+
+    public const ZM_S2S_ACCOUNT_ID = 'zmS2sAccountId';
+
+    public const ZM_S2S_CLIENT_ID = 'zmS2sClientId';
+
+    public const ZM_S2S_CLIENT_SECRET = 'zmS2sClientSecret';
+
+    public const ZM_S2S_CONFIG = 'zmS2sConfig';
+
+    public const ZM_S2S_OAUTH = 'zmS2sOauth';
+
+    public const ZM_S2S_OAUTH_ERRORED = 'zmS2sOauthErrored';
+
+    public const HMAC_SECRET = 'hmacSecret';
+}
---
+++ app/Constants/OauthConstant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Constants;
+
+final class OauthConstant
+{
+    public const ACCESS_TOKEN = 'access_token';
+
+    public const CLIENT_ID = 'client_id';
+
+    public const CLIENT_SECRET = 'client_secret';
+
+    public const EXPIRES_IN = 'expires_in';
+
+    public const EXPIRES_AT = 'expires_at';
+
+    public const GRANT_TYPE = 'grant_type';
+
+    public const REFRESH_TOKEN = 'refresh_token';
+
+    public const REFRESH_TOKEN_EXPIRES_IN = 'refresh_token_expires_in';
+
+    public const SCOPE = 'scope';
+
+    public const TOKEN_TYPE = 'token_type';
+}
